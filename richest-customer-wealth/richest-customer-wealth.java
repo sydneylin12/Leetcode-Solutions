7b1edef5322b54847490904326b9909b08c1a900
Sid Lin <sydney.lin12@gmail.com>
2020-12-04 17:27:27 -0600

    working commit - Created using LeetHub

---
+++ richest-customer-wealth/richest-customer-wealth.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int res = 0;
+        for(int[] in : accounts){
+            int sum = 0;
+            for(int i = 0; i < in.length; i++) sum += in[i];
+            res = Math.max(sum, res);
+        }
+        return res;
+    }
+}
