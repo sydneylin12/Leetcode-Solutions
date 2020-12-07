ea6f45578dea0b7a64ae9c3ec613de646b6c03ba
Sid Lin <sydney.lin12@gmail.com>
2020-12-07 10:44:57 -0600

    working commit - Created using LeetHub

--- concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.java
+++ concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.java
@@ -2,14 +2,12 @@ class Solution {
     public int concatenatedBinary(int n) {
         int MOD = 1000000007;
         int res = 0;
-        
         for(int i = 1; i <= n; i++){
             String s = Integer.toBinaryString(i);
             for(int j = 0; j < s.length(); j++){
                 res = (res * 2 + (s.charAt(j)-'0')) % MOD;
             }
         }
-        
         return res;
     }
 }
