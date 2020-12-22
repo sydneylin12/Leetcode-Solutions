668b4b84d32138e85cec87a7c48d6ad03d715ea2
Sid Lin <sydney.lin12@gmail.com>
2020-12-22 16:00:49 -0600

    Time: 79 ms (41.73%), Space: 54.7 MB (61.09%) - LeetHub

--- maximum-erasure-value/maximum-erasure-value.java
+++ maximum-erasure-value/maximum-erasure-value.java
@@ -1,5 +1,6 @@
 class Solution {
     public int maximumUniqueSubarray(int[] nums) {
+        // Return value and placeholder/local score value
         int res = 0, score = 0;
         int start = 0;
         Map<Integer, Integer> map = new HashMap<>();
@@ -9,13 +10,14 @@ class Solution {
             map.put(nums[end], map.getOrDefault(nums[end], 0)+1);
             score += nums[end];
 ​
+            // Use the while loop while the current number is not unique
+            // Will run at every index so previous numbers will be unique inside the map
             while(start < end && map.get(nums[end]) > 1){
+                // Start removing from HashMap
                 int st = nums[start++];
                 score -= st;
                 map.put(st, map.get(st)-1);
-                if(map.get(st) == 0) map.remove(st);
             }
-            
             res = Math.max(score, res);
         }
         return res;
